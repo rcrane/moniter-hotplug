eb4bb4ffd8e7cda719c516e162605cd8b3375b70
Robert Krahn <robert.krahn@tu-dresden.de>
2022-05-03 11:54:54 +0200

    bugfix for display port connection

--- monitor-hotplug.sh
+++ monitor-hotplug.sh
@@ -88,23 +88,23 @@ done <<< "$DEVICES"
 
 if [ -z "$DP1" ]
 then
-	xrandr --output DP-1 --off
+  xrandr --output DP-1 --off
 else
-	xrandr --output DP-1 --auto
+  xrandr --output DP-1 --auto
 fi
 
 if [ -z "$DP2" ]
 then
-	xrandr --output DP-2 --off
+  xrandr --output DP-2 --off
 else
-	xrandr --output DP-2 --auto
+  xrandr --output DP-2 --auto
 fi
 
 if [ -z "$HDMI1" ]
 then
-	xrandr --output HDMI-1 --off
+  xrandr --output HDMI-1 --off
 else
-	xrandr --output HDMI-1 --auto
+  xrandr --output HDMI-1 --auto
 fi
 
 
@@ -132,47 +132,79 @@ then
   if [ $(xrandr --prop | grep '00ffffffffffff001ab37f08e5490d00' | wc -l) -eq 1 ]
   then
     echo $( date +%H:%M:%S ) "--" "Showing desktop on HDMI-1 only" >> /tmp/udev-debug.log
-    xrandr --output HDMI-1 --mode 1920x1080 --primary --output eDP-1 --auto --below HDMI-1
+    xrandr --output HDMI-1 --mode 1920x1080 --primary --output eDP-1 --off
+    # xrandr --output HDMI-1 --mode 1920x1080 --primary --output eDP-1 --auto --below HDMI-1
   
   # Special case for 4K display with specific id
-  elif [ $(xrandr --prop | grep '00ffffffffffff00410c8fc116080000' | wc -l) -eq 1 ]
+  elif [ $(xrandr --prop | grep '00ffffffffffff00410c8fc116080000' | wc -l) -eq 1 ] || [ $(xrandr --prop | grep '091b0103803c2278ea3035a7554ea326' | wc -l) -eq 1 ]
   then
   
     echo $( date +%H:%M:%S ) "--" "Showing desktop on HDMI-1 only" >> /tmp/udev-debug.log
     xrandr --output HDMI-1 --mode 3840x2160 --primary --output eDP-1 --off
 
-	  if [ $USER == $TTYUSER ]
-	  then
-		echo "Xft.dpi: 170" > /home/${TTYUSER}/.Xresources
-	  else
-		su ${TTYUSER} -c "echo 'Xft.dpi: 170' > /home/${TTYUSER}/.Xresources"
-	  fi 
+    if [ $USER == $TTYUSER ]
+    then
+    echo "Xft.dpi: 170" > /home/${TTYUSER}/.Xresources
+    else
+    su ${TTYUSER} -c "echo 'Xft.dpi: 170' > /home/${TTYUSER}/.Xresources"
+    fi 
 
-	  echo "Xft.dpi: 170" | xrdb -load -
-	  randr --output HDMI-1 --primary
-	   
+    echo "Xft.dpi: 170" | xrdb -load -
+    xrandr --output HDMI-1 --primary
+     
   else
     echo $( date +%H:%M:%S ) "--" "Extending desktop to HDMI-1" >> /tmp/udev-debug.log
-	  xrandr --output HDMI-1 --auto --noprimary --output eDP-1 --auto --left-of HDMI-1 --primary
+    xrandr --output HDMI-1 --auto --noprimary --output eDP-1 --auto --left-of HDMI-1 --primary
   fi
-elif [ ! -z "$eDP1" -a -z "$HDMI1" ]; then
-  # If HDMI is not and internal display is present
-  echo $( date +%H:%M:%S ) "--" "Reducing desktop to internal screen" >> /tmp/udev-debug.log
-  xrandr --output eDP-1 --auto --primary --output HDMI-1 --off
-  
+     
 elif [ ! -z "$eDP1" -a ! -z "$DP1" -a -z "$DP2" ]; then
-  echo $( date +%H:%M:%S ) "--" "Extending desktop to DP-1" >> /tmp/udev-debug.log
-  xrandr --output DP-1 --auto --noprimary --output eDP-1 --auto --left-of DP-1 --primary
+  # If DP1 and internal display are present but DP2 is not
+
+  if [ $(xrandr --prop | grep '00ffffffffffff00410c8fc116080000' | wc -l) -eq 1 ] || [ $(xrandr --prop | grep '091b0103803c2278ea3035a7554ea326' | wc -l) -eq 1 ]
+  then
+  
+    echo $( date +%H:%M:%S ) "--" "Showing desktop on HDMI-1 only" >> /tmp/udev-debug.log
+    xrandr --output DP-1 --mode 3840x2160 --primary --output eDP-1 --off
+
+    if [ $USER == $TTYUSER ]
+    then
+    echo "Xft.dpi: 170" > /home/${TTYUSER}/.Xresources
+    else
+    su ${TTYUSER} -c "echo 'Xft.dpi: 170' > /home/${TTYUSER}/.Xresources"
+    fi 
+  
+    echo "Xft.dpi: 170" | xrdb -load -
+    xrandr --output DP-1 --primary
+  else
+    echo $( date +%H:%M:%S ) "--" "Extending desktop to DP-1" >> /tmp/udev-debug.log
+    xrandr --output DP-1 --auto --noprimary --output eDP-1 --auto --left-of DP-1 --primary
+  fi
   
 elif [ ! -z "$eDP1" -a ! -z "$DP2" -a -z "$DP1" ]; then
+    
   echo $( date +%H:%M:%S ) "--" "Extending desktop to DP-2" >> /tmp/udev-debug.log
   xrandr --output DP-2 --auto --noprimary --output eDP-1 --auto --left-of DP-2 --primary
   
 elif [ ! -z "$eDP1" -a ! -z "$DP1" -a ! -z "$DP2" ]; then
+  # If DP2 and internal display are present but DP1 is not
+
   echo $( date +%H:%M:%S ) "--" "Showing desktop on DP-1 and DP-2" >> /tmp/udev-debug.log
   xrandr --output DP-2 --auto --noprimary --output DP-1 --auto --left-of DP-2 --primary
+
+elif [ ! -z "$eDP1" -a -z "$HDMI1" ]; then
   
+  # If HDMI is not connected and internal display is present
+  echo $( date +%H:%M:%S ) "--" "Reducing desktop to internal screen" >> /tmp/udev-debug.log
+  xrandr --output eDP-1 --auto --primary --output HDMI-1 --off
+  
+elif [ ! -z "$eDP1" -a -z "$DP1" ]; then
+
+  # If DP1 is not connected and internal display is present
+  echo $( date +%H:%M:%S ) "--" "Reducing desktop to internal screen" >> /tmp/udev-debug.log
+  xrandr --output eDP-1 --auto --primary --output HDMI-1 --off
+
 else
+    
   echo $( date +%H:%M:%S ) "--" "Undefined monitor change detected" >> /tmp/udev-debug.log
   xrandr --auto
   sleep 5
@@ -189,53 +221,53 @@ echo $( date +%H:%M:%S ) "--" "Restarting OpenBox" >> /tmp/udev-debug.log
 
 if [ $USER == $TTYUSER ]
 then
-	openbox --restart
+  openbox --restart
 
-	killall pcmanfm-qt
-	pcmanfm-qt -d --desktop --profile=lxqt &
-	
-	killall lxqt-panel
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-    	lxqt-panel &
-	fi
-	
-	killall firefox 
-	pgrep -f /usr/lib/firefox | xargs kill
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-    	firefox &
-	fi
+  killall pcmanfm-qt
+  pcmanfm-qt -d --desktop --profile=lxqt &
+  
+  killall lxqt-panel
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    lxqt-panel &
+  fi
+  
+  killall firefox 
+  pgrep -f /usr/lib/firefox | xargs --no-run-if-empty kill
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    firefox &
+  fi
 
-	killall thunderbird 
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-    	thunderbird &
-	fi
+  killall thunderbird 
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    thunderbird &
+  fi
 
-	killall element-desktop 
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-    	element-desktop --hidden &
-	fi
+  killall element-desktop 
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    element-desktop --hidden &
+  fi
 
-	killall nm-applet
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-		sleep 2
-    	nm-applet &
-	fi
+  killall nm-applet
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    sleep 2
+    nm-applet &
+  fi
 
-	killall slack
-	retVal=$?
-	if [ $retVal -eq 0 ]; then
-    	slack -u -s &
-	fi
+  killall slack
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    slack -u -s &
+  fi
 
-	killall lxqt-notificationd
+  killall lxqt-notificationd
 
 else
-	su ${TTYUSER} -c "openbox --restart"
+  su ${TTYUSER} -c "openbox --restart"
 fi
 
 
